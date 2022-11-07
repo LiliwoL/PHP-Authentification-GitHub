c1c401f9013d515af7605b7033ebc43ae53a9a33
LiliwoL <github@liliwol.fr>
2022-11-07 15:02:10 +0100

    Add

--- User.class.php
+++ User.class.php
@@ -16,7 +16,6 @@ class User
             // Connect to the database
             try {
                 $this->db = new PDO($this->dsn);
-                var_dump($this->db->query("SELECT * FROM users;"));
             }catch (PDOException $e)
             {
                 die ('DB Error');
