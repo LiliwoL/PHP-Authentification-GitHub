e3516d2347b0f6e37f0ba3eca05a9554ca243707
LiliwoL <github@liliwol.fr>
2023-02-20 14:47:21 +0100

    .env files

--- sql/users.sql
+++ sql/users.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `users` (
-                         `id` int(11) NOT NULL ,
-                         `oauth_provider` TEXT CHECK( oauth_provider IN ('github', 'facebook', 'google', 'twitter')) NOT NULL DEFAULT 'github',
-                         `oauth_uid` varchar(50) NOT NULL,
-                         `name` varchar(50) NOT NULL,
-                         `username` varchar(50) NOT NULL,
-                         `email` varchar(100)  NOT NULL,
-                         `location` varchar(50)  DEFAULT NULL,
-                         `picture` varchar(255)  DEFAULT NULL,
-                         `link` varchar(255)  DEFAULT NULL,
-                         `created` TEXT NOT NULL DEFAULT (DATETIME('now')),
-                         `modified` TEXT NOT NULL DEFAULT (DATETIME('now')),
-                         PRIMARY KEY (`id`)
+    `id` int(11) NOT NULL ,
+    `oauth_provider` TEXT CHECK( oauth_provider IN ('github', 'facebook', 'google', 'twitter')) NOT NULL DEFAULT 'github',
+    `oauth_uid` varchar(50) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `username` varchar(50) NOT NULL,
+    `email` varchar(100)  NOT NULL,
+    `location` varchar(50)  DEFAULT NULL,
+    `picture` varchar(255)  DEFAULT NULL,
+    `link` varchar(255)  DEFAULT NULL,
+    `created` TEXT NOT NULL DEFAULT (DATETIME('now')),
+    `modified` TEXT NOT NULL DEFAULT (DATETIME('now')),
+    PRIMARY KEY (`id`)
 );
